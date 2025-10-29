02d3e40f372e1ff034b7e2cef88d8a8fb199d567
promit1201 <bharpromit@gmail.com>
2025-10-30 01:46:15 +0530

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
-import OnboardingChatbot from '@/components/OnboardingChatbot';
 import { HomeWorkouts } from '@/components/HomeWorkouts';
 
 const Home = () => {
-  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
-
   return (
     <div className="min-h-screen">
       <Navbar />
       <Hero />
       <HomeWorkouts />
       <Footer />
-      <OnboardingChatbot 
-        isOpen={isChatbotOpen} 
-        onClose={() => setIsChatbotOpen(false)} 
-      />
     </div>
   );
 };
